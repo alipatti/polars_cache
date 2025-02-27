86167af503948443c70db0fe5c31ffe301449de2
Alistair Pattison <alistairpattison@gmail.com>
2025-02-27 09:53:57 -0500

    handle functions with no kwargs

--- polars_cache/helpers.py
+++ polars_cache/helpers.py
@@ -6,4 +6,4 @@ def args_as_dict[**P](f: Callable[P, Any], *args: P.args, **kwargs: P.kwargs):
     signature = inspect.signature(f)
 
     # positional arguments, default kwargs, passed kwargs
-    return dict(zip(signature.parameters, args)) | f.__kwdefaults__ | kwargs
+    return dict(zip(signature.parameters, args)) | (f.__kwdefaults__ or {}) | kwargs
