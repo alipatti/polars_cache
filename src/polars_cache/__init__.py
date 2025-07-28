81b17415dc389be5673ec1ab65d7fee42945a23e
Alistair Pattison <alistairpattison@gmail.com>
2025-07-27 21:10:23 -0400

    massive rewrite

--- src/polars_cache/__init__.py
+++ src/polars_cache/__init__.py
@@ -1,9 +1,164 @@
-from polars_cache.old_cache import CachedFunction, _extract_kwargs
+from datetime import datetime, timedelta
+from pathlib import Path
+import hashlib
+import shutil
+from typing import Any
 
-cache_ldf = _extract_kwargs(CachedFunction)
-cache = _extract_kwargs(CachedFunction)
+import polars as pl
 
-__all__ = [
-    "cache",
-    "cache_ldf",  # TODO: depricate this
-]
+__all__ = ["cache_to_disc"]
+
+_HASH_FUNCTION: str = "md5"
+
+
+def cache_to_disc(
+    query: pl.LazyFrame,
+    *,
+    base_directory: str | Path = ".polars_cache/",
+    max_age: timedelta | int | None = None,
+    verbose=False,
+    # options to pass on
+    write_parquet_options: dict[str, Any] = {},
+    read_parquet_options: dict[str, Any] = {},
+    collect_options: dict[str, Any] = {},
+) -> pl.LazyFrame:
+    """
+    Cache this LazyFrame to disc when `.collect()` is called (possibly far downstream).
+    The cached result will be reused on subsequent invocations of `.collect()`, even across
+    Python sessions.
+
+    **WARNING:** This function is opaque to the Polars optimizer and therefore will block
+    predicate pushdown, projection pushdown, and other optmizations. Use with caution.
+
+    Parameters
+    ----------
+    query
+        The LazyFrame to cache.
+
+    base_directory
+        The directory where cache files are stored. Defaults to `.polars_cache` in the current directory.
+
+    max_age
+        Maximum age at which the cache is considered valid (in seconds if integer).
+        If `None`, the cache never expires. Set to zero to force a refresh.
+
+    write_parquet_options
+        Options to pass to `query.write_parquet` when writing the cache.
+
+    read_parquet_options
+        Options to pass to `pl.read_parquet` when reading from the cache.
+
+    collect_options
+        Options to pass to `query.collect`.
+
+    Returns
+    -------
+    pl.LazyFrame
+        A LazyFrame. When `collect()` is called, it will load from the cache if
+        it exists and has not expired. If not, it will evaluate, cache, and
+        return the result.
+
+    Notes
+    -----
+
+    The query is identified by a hash of `query.serialize()`, and therefore is not
+    guaranteed to be stable across versions of Polars.
+
+    Examples
+    --------
+    >>> import polars as pl
+    >>> lf = (
+    >>>     pl.LazyFrame({"x": [1, 2, 3]})
+    >>>     .with_columns(very_expensive_compuation(x))
+    >>>     .pipe(cache_to_disc, max_age=120) # in seconds
+    >>> )
+    >>>
+    >>> df1 = query.collect()  # populate the cache
+    >>> df2 = query.collect()  # second invocation will be much faster!
+    """
+
+    cache_location = _cache_location(query, base_directory)
+
+    def on_collect() -> pl.DataFrame:
+        """Function that gets called when the LazyFrame is collected."""
+
+        # use the cache if it's valid
+        if _valid_cache(cache_location, max_age, verbose):
+            if verbose:
+                print(f"CACHE: restoring from {cache_location}")
+
+            return pl.read_parquet(cache_location, **read_parquet_options)
+
+        if verbose:
+            print(f"CACHE: creating at {cache_location}")
+
+        # if doesn't exist or isn't valid, then collect the query
+        df = query.collect(**collect_options)
+
+        # delete existing cache
+        _remove_cache(cache_location)
+
+        # write new cache
+        cache_location.parent.mkdir(parents=True, exist_ok=True)
+        df.write_parquet(cache_location, **write_parquet_options)
+
+        # return collected df
+        return df
+
+    return pl.defer(
+        on_collect,
+        schema=lambda: query.collect_schema(),
+        validate_schema=True,
+    )
+
+
+def _cache_location(
+    query: pl.LazyFrame,
+    base_directory: str | Path,
+    hash_length=20,
+) -> Path:
+    hash = hashlib.new(_HASH_FUNCTION, query.serialize()).hexdigest()[:hash_length]
+    return Path(base_directory) / hash
+
+
+def _remove_cache(cache_location: Path):
+    if cache_location.is_dir():
+        shutil.rmtree(cache_location)
+    else:
+        cache_location.unlink(missing_ok=True)
+
+
+def _valid_cache(
+    cache_location: Path,
+    max_age: timedelta | int | None,
+    verbose: bool = False,
+) -> bool:
+    if not cache_location.exists():
+        if verbose:
+            print(f"CACHE: doesn't exist {cache_location}")
+
+        return False  # cache doesn't exist
+
+    if max_age is None:
+        if verbose:
+            print(f"CACHE: found (doesn't expire) {cache_location}")
+
+        return True  # cache doesn't expire
+
+    # convert max_age to timedelta
+    if isinstance(max_age, int):
+        max_age = timedelta(seconds=max_age)
+
+    cache_creation_time = datetime.fromtimestamp(cache_location.stat().st_mtime)
+    cache_age = datetime.now() - cache_creation_time
+
+    if cache_age > max_age:
+        if verbose:
+            print(f"CACHE: expired (age={cache_age.total_seconds()}s) {cache_location}")
+
+        return False  # cache expired
+
+    if verbose:
+        print(f"CACHE: found (age={cache_age.total_seconds()}s) {cache_location}")
+
+    return True
